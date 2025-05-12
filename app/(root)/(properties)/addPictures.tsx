7372a3607c877870152f404e8654e8851ac5be9f
Jorge <jorgevivar2005@gmail.com>
2025-05-13 00:16:22 +0200

    Implement image upload functionality in AddPictures component
    - Added a new function to handle image uploads to the server, utilizing FormData for image processing.
    - Integrated propertyId retrieval using useLocalSearchParams for dynamic property handling.
    - Updated the Continue button to trigger the image upload process, enhancing user interaction and functionality.
    (does not work)

--- app/(root)/(properties)/addPictures.tsx
+++ app/(root)/(properties)/addPictures.tsx
@@ -1,20 +1,65 @@
 import React, { useState } from 'react'
 import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
 
-import { router } from 'expo-router'
+import { router, useLocalSearchParams } from 'expo-router'
 
 import ImageCarousel from '@/components/selectImages/ImageCarousel'
 import ImageSelector from '@/components/selectImages/ImageSelector'
+import { INSTARENT_API_KEY, INSTARENT_API_URL } from '@/utils/constants'
 
 type AppImage = {
   uri: string
 }
 
 const AddPictures = () => {
+  const params = useLocalSearchParams()
+
   const SELECTIONLIMIT = 30
+  const propertyId = '1nk1B3XrIQ0FNIG9MqkNZFod3t9Qiut2' // params.propertyId (usar params en lugar de hardcodear)
 
   const [images, setImages] = useState<AppImage[]>([])
   const disabledSelect = images.length >= SELECTIONLIMIT
+
+  async function addImage(propertyData: { image: any; propertyId: string }) {
+    try {
+      const formData = new FormData()
+
+      let imageBlob
+      if (typeof propertyData.image === 'string') {
+        const response = await fetch(propertyData.image)
+        const blob = await response.blob()
+        imageBlob = blob
+      } else {
+        imageBlob = propertyData.image
+      }
+
+      if (!imageBlob) {
+        throw new Error('Invalid image')
+      }
+
+      formData.append('image', imageBlob)
+      formData.append('property_id', propertyData.propertyId)
+
+      const response = await fetch(`${INSTARENT_API_URL}/images/new`, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${INSTARENT_API_KEY}`
+        },
+        body: formData
+      })
+
+      const data = await response.json()
+
+      if (!response.ok) {
+        throw new Error(data.error || 'Error adding new Image')
+      }
+
+      return data
+    } catch (error) {
+      console.error('Error adding new Image', error)
+      throw error
+    }
+  }
 
   const handleSelectImages = (newImages: AppImage[]) => {
     const availableSlots = SELECTIONLIMIT - images.length
@@ -25,6 +70,21 @@ const AddPictures = () => {
 
   const handleRemoveImage = (uri: string) => {
     setImages((prev) => prev.filter((img) => img.uri !== uri))
+  }
+
+
+  const handleAddImage = async () => {
+    if (images.length >= 1) {
+      try {
+        for (const image of images) {
+          await addImage({ image: image, propertyId })
+        }
+
+        router.back()
+      } catch (error) {
+        alert('Error adding images. Please try again.')
+      }
+    }
   }
 
   return (
@@ -59,10 +119,7 @@ const AddPictures = () => {
             className={`w-[48%] h-16 flex-row items-center justify-center rounded-xl p-4 ${
               images.length === 0 ? 'bg-gray-400' : 'bg-darkBlue'
             }`}
-            onPress={() => {
-              if (images.length > 0) {
-              }
-            }}
+            onPress={handleAddImage}
             disabled={images.length === 0}>
             <Text className="text-base font-semibold text-white">Continue</Text>
           </TouchableOpacity>
