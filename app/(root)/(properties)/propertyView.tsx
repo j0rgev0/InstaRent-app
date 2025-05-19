f70250f91ca85493255f44486d18e3014f97baae
Jorge <jorgevivar2005@gmail.com>
2025-05-19 19:12:11 +0200

    Enhance PropertyView component with animated image carousel and dot indicators
    - Integrated an animated FlatList for property images, allowing for smooth horizontal scrolling.
    - Added animated dot indicators to visually represent the current image in the carousel.
    - Improved layout and styling for better user experience, including adjustments to property address display.
    - Updated loading indicator size for consistency.

--- app/(root)/(properties)/propertyView.tsx
+++ app/(root)/(properties)/propertyView.tsx
@@ -2,11 +2,11 @@ import { INSTARENT_API_KEY, INSTARENT_API_URL } from '@/utils/constants'
 import { Property } from '@/utils/types'
 import { Ionicons } from '@expo/vector-icons'
 import { useLocalSearchParams, useNavigation } from 'expo-router'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import {
   ActivityIndicator,
+  Animated,
   Dimensions,
-  FlatList,
   Image,
   RefreshControl,
   ScrollView,
@@ -20,6 +20,11 @@ import MapPreview from '@/components/map/MapPreview'
 
 const { width } = Dimensions.get('window')
 
+const VISIBLE_DOTS = 5
+const DOT_SIZE = 8
+const DOT_SPACING = 4
+const DOT_CONTAINER_WIDTH = VISIBLE_DOTS * (DOT_SIZE + DOT_SPACING)
+
 const PropertyView = () => {
   const navigation = useNavigation()
   const params = useLocalSearchParams()
@@ -27,6 +32,11 @@ const PropertyView = () => {
   const propertyId = params.propertyId
   const [property, setProperty] = useState<Property>()
   const [refreshing, setRefreshing] = useState(false)
+
+  const scrollX = useRef(new Animated.Value(0)).current
+
+  // Para mover la vista de puntos
+  const dotsScrollX = useRef(new Animated.Value(0)).current
 
   const fetchProperty = async () => {
     if (!propertyId) return
@@ -44,6 +54,7 @@ const PropertyView = () => {
       if (!response.ok) throw new Error(data.error || 'Error getting property')
 
       setProperty(data)
+      dotsScrollX.setValue(0)
     } catch (error) {
       console.error('Error getting property', error)
     }
@@ -55,14 +66,40 @@ const PropertyView = () => {
     setRefreshing(false)
   }
 
+  const onScroll = (event: any) => {
+    const offsetX = event.nativeEvent.contentOffset.x
+    const index = Math.round(offsetX / width)
+
+    if (property) {
+      const startIndex = Math.max(
+        0,
+        Math.min(index - Math.floor(VISIBLE_DOTS / 2), property.images.length - VISIBLE_DOTS)
+      )
+
+      Animated.spring(dotsScrollX, {
+        toValue: startIndex * (DOT_SIZE + DOT_SPACING),
+        useNativeDriver: true,
+        friction: 10,
+        tension: 40
+      }).start()
+    }
+  }
+
   useEffect(() => {
     fetchProperty()
   }, [propertyId])
 
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitleAlign: 'center',
+      headerTitle: 'Property Details'
+    })
+  }, [navigation])
+
   if (!property) {
     return (
       <View className="flex-1 justify-center items-center">
-        <ActivityIndicator size="large" color="#353949" />
+        <ActivityIndicator size="small" color="#353949" />
       </View>
     )
   }
@@ -71,22 +108,79 @@ const PropertyView = () => {
     <ScrollView
       refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
       className="bg-white">
-      <FlatList
-        data={property.images}
-        horizontal
-        pagingEnabled
-        showsHorizontalScrollIndicator={false}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <Image source={{ uri: item.url }} style={{ width, height: 300 }} resizeMode="cover" />
-        )}
-      />
+      <View>
+        <Animated.FlatList
+          data={property.images}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Image source={{ uri: item.url }} style={{ width, height: 300 }} resizeMode="cover" />
+          )}
+          onScroll={Animated.event([{ nativeEvent: { contentOffset: { x: scrollX } } }], {
+            useNativeDriver: false,
+            listener: onScroll
+          })}
+          scrollEventThrottle={16}
+        />
+
+        <View
+          className="overflow-hidden justify-center align-middle absolute mt-2 mb-3 bg-black/50 bottom-1 rounded-full"
+          style={{
+            height: DOT_SIZE * 3,
+            width: DOT_CONTAINER_WIDTH,
+            left: (width - DOT_CONTAINER_WIDTH) / 2
+          }}>
+          <Animated.View
+            style={{
+              flexDirection: 'row',
+              transform: [
+                {
+                  translateX: dotsScrollX.interpolate({
+                    inputRange: [0, property.images.length * (DOT_SIZE + DOT_SPACING)],
+                    outputRange: [0, -property.images.length * (DOT_SIZE + DOT_SPACING)],
+                    extrapolate: 'clamp'
+                  })
+                }
+              ]
+            }}>
+            {property.images.map((_, index) => {
+              const inputRange = [(index - 1) * width, index * width, (index + 1) * width]
+              const scaleAnim = scrollX.interpolate({
+                inputRange,
+                outputRange: [0.5, 0.8, 0.5],
+                extrapolate: 'clamp'
+              })
+              const colorAnim = scrollX.interpolate({
+                inputRange,
+                outputRange: ['#bbb', '#fff', '#bbb'],
+                extrapolate: 'clamp'
+              })
+
+              return (
+                <Animated.View
+                  key={index}
+                  style={{
+                    height: DOT_SIZE,
+                    width: DOT_SIZE,
+                    borderRadius: DOT_SIZE / 2,
+                    backgroundColor: colorAnim,
+                    marginHorizontal: DOT_SPACING / 2,
+                    transform: [{ scale: scaleAnim }]
+                  }}
+                />
+              )
+            })}
+          </Animated.View>
+        </View>
+      </View>
 
       <View className="p-4">
         <Text className="text-xl font-bold text-black mb-1 capitalize">
           {property.type}
           <Text className="normal-case"> in </Text>
-          {property.street}, {property.locality}
+          {property.street} {property.street_number}, {property.locality}
         </Text>
         <Text className="text-lg text-[#353949] font-semibold mb-2">
           €{property.price.toLocaleString()} {property.operation === 'sell' ? '' : '/ month'}
